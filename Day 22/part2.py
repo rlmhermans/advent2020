3f58abd053e6c01d087fa1cb723e67efb187c8f0
Ruud Hermans <rlmhermans@gmail.com>
2020-12-22 12:46:20 +0100

    Day 22 part 2

--- Day 22/part2.py
+++ Day 22/part2.py
@@ -1,7 +1,7 @@
-deckone = [9, 2, 6, 3, 1]
-decktwo = [5, 8, 4, 7, 10]
-# deckone = [13, 25, 18, 6, 42, 8, 37, 27, 44, 38, 10, 28, 50, 5, 16, 47, 30, 29, 39, 21, 2, 4, 12, 35, 32]
-# decktwo = [20, 14, 46, 34, 7, 26, 15, 43, 36, 49, 11, 23, 31, 48, 1, 19, 45, 22, 24, 40, 41, 17, 33, 9, 3]
+# deckone = [9, 2, 6, 3, 1]
+# decktwo = [5, 8, 4, 7, 10]
+deckone = [13, 25, 18, 6, 42, 8, 37, 27, 44, 38, 10, 28, 50, 5, 16, 47, 30, 29, 39, 21, 2, 4, 12, 35, 32]
+decktwo = [20, 14, 46, 34, 7, 26, 15, 43, 36, 49, 11, 23, 31, 48, 1, 19, 45, 22, 24, 40, 41, 17, 33, 9, 3]
 
 def score(deck):
     points = 0
@@ -10,17 +10,40 @@ def score(deck):
         points += (i + 1) * n
     return points
 
-while deckone and decktwo: 
-    numberone = deckone.pop(0)
-    numbertwo = decktwo.pop(0)
+def game(deckone, decktwo):
+    previousconfig = set()
+    while deckone and decktwo:
+        configone = ','.join([str(i) for i in deckone])
+        configtwo = ','.join([str(i) for i in decktwo])
+        config = configone + ';' + configtwo
+        if not config in previousconfig:
+            previousconfig.add(config)
+            numberone = deckone.pop(0)
+            numbertwo = decktwo.pop(0)
 
-    if numberone > numbertwo:
-        deckone += [numberone, numbertwo]
-    else:
-        decktwo += [numbertwo, numberone]
+            winner = 0
+            if len(deckone) >= numberone and len(decktwo) >= numbertwo:
+                winner = game(deckone[:numberone], decktwo[:numbertwo])
+                
+            elif numberone > numbertwo:
+                winner = 1
+            
+            if winner:
+                deckone += [numberone, numbertwo]
+            else:
+                decktwo += [numbertwo, numberone]
+
+            if not deckone:
+                return 0
+            if not decktwo:
+                return 1
+        else:
+            return 1
+
+winner = game(deckone, decktwo)
 
 points = 0
-if deckone:
+if winner == 1:
     points = score(deckone)
 
 else:
